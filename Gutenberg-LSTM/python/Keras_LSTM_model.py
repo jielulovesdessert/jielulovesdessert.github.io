14556e0f4078c34eef5795cfc3c8fefa041d660a
Jie Lu <lu.jie@columbia.edu>
2018-11-23 01:59:31 -0500

    update scripts

--- Gutenberg-LSTM/python/Keras_LSTM_model.py
+++ Gutenberg-LSTM/python/Keras_LSTM_model.py
@@ -1,6 +1,6 @@
 import keras
 
-max_len = 40
+max_len = 25
 num_words = 1000
 embedding_size = 32
 n_classes = 3
--- Gutenberg-LSTM/python/Preprocessing.py
+++ Gutenberg-LSTM/python/Preprocessing.py
@@ -35,7 +35,7 @@ def unison_shuffled_copies(a, b):
 x_train, y_train = unison_shuffled_copies(x_train, y_train)
 
 #tokenize x
-max_len = 40
+max_len = 25
 num_words = 1000
 from keras.preprocessing.text import Tokenizer
 # Fit the tokenizer on the training data
